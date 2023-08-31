7e29952f78bbbe23898f5f5551a60691229f685b
Henry Jiang <henry.jiang@net-effect.com>
2023-08-31 10:04:44 -0400

    0831 10am

--- src/app/app-components/categories/categories.component.ts
+++ src/app/app-components/categories/categories.component.ts
@@ -15,7 +15,7 @@ interface categoryData {
 
 
 export class CategoriesComponent implements OnInit {
-
+  changeShow: boolean[] = [];
   changeColor: boolean[] = [];
   selected:boolean = false;
   searchWord : string = "";
@@ -38,109 +38,140 @@ export class CategoriesComponent implements OnInit {
     }
     ]
     };
-  list : string[] = ["AAA","BBB"];
 
-  categories : string[] = []
+    lib2 = {
+      categories: [
+      'Performance', 'Investments', 'Operations'
+      ],
+      applets: [
+      {
+      name: 'Performance Snapshot',
+      categories: ['Performance']
+      },
+      {
+      name: 'Commitment Widget',
+      categories: ['Investments']
+      },
+      {
+      name: 'CMS',
+      categories: ['Investments', 'Performance' ]
+      }
+      ]
+      };
+ 
 
 
-
-  categories1 : categoryData[] = []
+  categories  : categoryData[] = []
 
 
    applets : string[] = []
-  ngOnInit(): void {
-     // this.categories = this.lib.categories;
-      this.lib.categories.forEach((t1,i)=>{
 
-        console.log("categories  AAAAA ---  ", t1);
-
-        let names = this.lib.applets.filter(t=>t.categories.includes(t1)).map(t=>t.name);
-
-        console.log("categories  AAAAA ---  ", {t1, names});
-
-        this.categories1.push({category:t1, names:names});
-        
-
-      });
-
-      this.categories1.forEach((t,i)=>{
-        this.changeColor.push(false);
-       });
-      let applets1  = this.lib.applets;
-
-      console.log("categories   ", this.categories);
-
-      console.log("applets1   ",  applets1); 
+   constructor(){
+ 
+    //  this.addBigData(this.lib, 100, 5000);
       
+   }
+  ngOnInit(): void { 
+   this.initData();   
   }
 
+  initData(){
+    this.lib.categories.forEach((t1,i)=>{ 
+      let names = this.lib.applets.filter(t=>t.categories.includes(t1)).map(t=>t.name); 
+      
+      this.categories.push({category:t1, names:names}); 
+
+    });
+
+    this.categories.forEach((t,i)=>{
+      this.changeColor.push(false);
+      this.changeShow.push(true);
+     });
+     this.categories = this.categories.sort((a,b)=>a.category.localeCompare(b.category));
+
+  }
+  
   seletCategory(category:string, index:number){
     
-    console.log("selet Category   index",  {category, index});
-    console.log("this.changeColor -----  AA ", this.changeColor);
+    //console.log("selet Category   index",  {category, index});
+    //console.log("this.changeColor -----  AA ", this.changeColor);
 
     //let c1 = [...this.changeColor]
 
     this.changeColor = this.changeColor.map(t=>t=false);
 
+
+
     this.changeColor[index] = true;
-  //  c1.forEach((t,i)=>
-  //     {
-  //       if(i == index){          
-  //         t = true
-  //         //console.log("forEach  AAA true--- ",  {t, i});
-  //       }else{
-  //         t= false
-  //         //console.log("forEach  NOT true--- ", {t, i});
-  //       }
-        
-  //       console.log("forEach RRR  index ", {t, i}); 
-  //     }
-  //     );
-     /// console.log("this.changeColor -----  BB ", this.changeColor);
-    setTimeout(() => { 
-    //  this.changeColor[index] =true;
-      console.log("this.changeColor -----  CC ",this.changeColor);
-     }, 100);
+ 
+  //   setTimeout(() => { 
+  //   //  this.changeColor[index] =true;
+  //     console.log("this.changeColor -----  CC ",this.changeColor);
+  //    }, 100);
      
-   // 
-    console.log("this.changeColor -----   B ", this.changeColor);
+  //  // 
+  //   console.log("this.changeColor -----   B ", this.changeColor);
 
     let result = this.lib.applets.filter(t=>t.categories.includes(category)).map(t=>t.name);
 
     this.applets = result;
-    console.log("result   ", result);
-
+    //console.log("result   ", result);
+  
   
    //  this.lib.applets["category"]
   }
 
   searchApplets(event: any){
-    console.log("searchApplets applet---------   ", event.target.value);
-    console.log("searchWord ---------   ", this.searchWord);
-    
+ 
+    let cats = this.categories.map(t=>t.category);
     if(event.target.value){
 
       let applet  = event.target.value;
-      let result = this.lib.applets.filter(t=>t.name.toLowerCase().includes(applet.toLowerCase())).map(t=>t.name);
-      this.applets = result;
+       this.applets= this.lib.applets.filter(t=>t.name.toLowerCase().includes(applet.toLowerCase())).map(t=>t.name);
+       let r1 = this.lib.applets.filter(t=>t.name.toLowerCase().includes(applet.toLowerCase())).map(t=>t.categories.concat(t.name));
+       
+ 
+      cats.forEach((t1,i)=>{ 
+        this.changeShow[i] = false;  
+                r1.forEach((t2 )=>{  
+                   t2.some((t3)=>{ 
+                      
+                      if(t3.toLowerCase().includes(t1.toLowerCase()) ){   
+                       this.changeShow[i] = true;
+                      } 
+                   });
+                 
 
-      console.log("applets   ", result);
+                });
+         
+
+      });
+    }else{
+
+      cats.forEach((t1,i)=>{
+        this.changeShow[i] = true;
+      });
+
     }
   }
+  addBigData(lib: any, ncategs: number, napplets: number) {
+    for (var i = 0; i < ncategs; i++) {
+      lib.categories.push('Sample Category ' + i);
+    }
+    var n = lib.categories.length;
+    for (var i = 0; i < napplets; i++) {
+      var a : any = {
+        name: 'CMS' + i,
+        categories: []
+      };
+      for (var j = 0; j < Math.floor(Math.random() * 10); ++j) {
+        var idx = Math.floor(Math.random() * n) % n;
+        let a1 = lib.categories[idx];
+        a.categories.push(a1);
+      }
+      lib.applets.push(a);
+    }
 
-  // changeListColor(index:number){
-  //   console.log("changeListColor -----A  index ",index);
-  //   this.changeColor.forEach(t=>
-  //     {
-  //       t=false
-
-  //       console.log("forEach  index ",t);
-  //     }
-  //     );
-
-
-  //   this.changeColor[index] =true;
-  //   console.log("this.changeColor -----   ", this.changeColor);
-  // }
+    //const slicedArray = array.slice(0, n);
+    }
 }
